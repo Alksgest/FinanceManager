ab60e3c0f2f2b41bab69005493b204289028e18a
vlky <vlky@PC-VLKY>
2019-12-12 15:20:44 +0200

    added tests folder, added nunit packages, commented main method

--- FinanceManager/Program.cs
+++ FinanceManager/Program.cs
@@ -8,16 +8,14 @@ namespace FinanceManager
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            GlobalSettings.LoadSettings();
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
 
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-
-        }
+        //}
     }
 }
---
+++ FinanceManager/Tests/RepositoriesTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace FinanceManager.Tests
+{
+    [TestFixture]
+    class RepositoriesTests 
+    {
+        [Test]
+        public void ReasonsRespoTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
